2211f3c85145c2caf4eb8280f3cd0821423938be
NarePirumyan <narepirumyan@mail.ru>
2023-02-28 15:03:34 +0400

    added hi func

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ function test() {
   return "hello";
 }
 
+function hi() {
+  return(test());
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
